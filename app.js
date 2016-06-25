4bf7d3086cae2d1e87a93068b29cbca01c5fabb6
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-25 16:32:55 -0500

    Store Multiple Values in one Variable using JavaScript Arrays Complete

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+Store Multiple Values in one Variable using JavaScript Arrays Complete
+Nest one Array within Another Array Complete
+Access Array Data with Indexes Complete
+Modify Array Data With Indexes Complete
+Access Multi-Dimensional Arrays With Indexes Complete
+Manipulate Arrays With push() Complete
+Manipulate Arrays With pop() Complete
+Manipulate Arrays With shift() Complete
+Manipulate Arrays With unshift() Complete
+Shopping List Complete
+Write Reusable JavaScript with Functions Complete
+Passing Values to Functions with Arguments Complete
+Global Scope and Functions Complete
+Local Scope and Functions Complete
+Global vs. Local Scope in Functions Complete
+Return a Value from a Function with Return Complete
+Assignment with a Returned Value Complete
+Stand in Line
+*/
+var array = ["John", 23];
+var array = ["Juan", 32];
